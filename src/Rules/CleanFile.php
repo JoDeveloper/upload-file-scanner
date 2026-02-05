76f0c65dadaa183699876be9abb6506a0e8496a9
yelbasheer <jodeveloper8@gmail.com>
2026-02-05 08:12:19 +0300

    fix: Update error message for unresolvable file path in CleanFile rule.

--- src/Rules/CleanFile.php
+++ src/Rules/CleanFile.php
@@ -19,12 +19,7 @@ class CleanFile implements ValidationRule
         $path = $value->getRealPath();
 
         if ($path === false) {
-<<<<<<< HEAD
             $fail('The file could not be scanned.');
-=======
-            $fail('The uploaded file contains a virus.');
-
->>>>>>> fa131f582866ac514d38cd454f3a8f0d46b76a05
             return;
         }
 
